50d014763e227ffd310267cd497600506558e926
R.M.Bineth Geenuka <143432257+Geenukabineth@users.noreply.github.com>
2024-06-05 19:42:44 +0530

    Update MainActivity.java

--- app/src/main/java/com/uombit/weatherapp/MainActivity.java
+++ app/src/main/java/com/uombit/weatherapp/MainActivity.java
@@ -75,79 +75,79 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(String response) {
 
                     try {
-                        //find temperature
+                       
                         JSONObject jsonObject = new JSONObject(response);
                         JSONObject object = jsonObject.getJSONObject("main");
                         double temp = object.getDouble("temp");
                         temptv.setText("Temperature\n"+temp+"°C");
 
-                        //find country
+                        
                         JSONObject object8 = jsonObject.getJSONObject("sys");
                         String count = object8.getString("country");
                         country.setText(count+"  :");
 
-                        //find city
+                        
                         String city = jsonObject.getString("name");
                         city_nam.setText(city);
 
-                        //find icon
+                        
                         JSONArray jsonArray = jsonObject.getJSONArray("weather");
                         JSONObject obj = jsonArray.getJSONObject(0);
                         String icon = obj.getString("icon");
                         Picasso.get().load("http://openweathermap.org/img/wn/"+icon+"@2x.png").into(imageView);
 
-                        //find date & time
+                        
                         Calendar calendar = Calendar.getInstance();
                         SimpleDateFormat std = new SimpleDateFormat("HH:mm a \nE, MMM dd yyyy");
                         String date = std.format(calendar.getTime());
                         time.setText(date);
 
-                        //find latitude
+                        
                         JSONObject object2 = jsonObject.getJSONObject("coord");
                         double lat_find = object2.getDouble("lat");
                         latitude.setText(lat_find+"°  N");
 
-                        //find longitude
+                       
                         JSONObject object3 = jsonObject.getJSONObject("coord");
                         double long_find = object3.getDouble("lon");
                         longitude.setText(long_find+"°  E");
 
-                        //find humidity
+                        
                         JSONObject object4 = jsonObject.getJSONObject("main");
                         int humidity_find = object4.getInt("humidity");
                         humidity.setText(humidity_find+"  %");
 
-                        //find sunrise
+                       
                         JSONObject object5 = jsonObject.getJSONObject("sys");
                         String sunrise_find = object5.getString("sunrise");
                         sunrise.setText(sunrise_find+"  am");
 
-                        //find sunrise
+                        
                         JSONObject object6 = jsonObject.getJSONObject("sys");
                         String sunset_find = object6.getString("sunset");
                         sunset.setText(sunset_find+"  pm");
 
-                        //find pressure
+                        
                         JSONObject object7 = jsonObject.getJSONObject("main");
                         String pressure_find = object7.getString("pressure");
                         pressure.setText(pressure_find+"  hPa");
 
-                        //find wind speed
+                        
                         JSONObject object9 = jsonObject.getJSONObject("wind");
                         String wind_find = object9.getString("speed");
                         wind.setText(wind_find+"  km/h");
 
-                        //find min temperature
+                        
                         JSONObject object10 = jsonObject.getJSONObject("main");
                         double mintemp = object10.getDouble("temp_min");
                         min_temp.setText("Min Temp\n"+mintemp+" °C");
 
-                        //find max temperature
+                        
                         JSONObject object12 = jsonObject.getJSONObject("main");
                         double maxtemp = object12.getDouble("temp_max");
                         max_temp.setText("Max Temp\n"+maxtemp+" °C");
 
-                        //find feels
+                        
                         JSONObject object13 = jsonObject.getJSONObject("main");
                         double feels_find = object13.getDouble("feels_like");
                         feels.setText(feels_find+" °C");
